7cda13e9f10ad30eb14abd8161b872f8540af99b
Henry Robbins <hwr26@cornell.edu>
2021-09-10 13:15:25 -0400

    Added work from channel series to archive.

--- netpbm/channel/channel.py
+++ netpbm/channel/channel.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import ceil, pi
+from math import pi
 from typing import Callable
 from dmtools import netpbm
 from dmtools import colorspace
@@ -24,31 +24,16 @@ def channel(image:netpbm.Netpbm, f_R:Callable,
     Returns:
         netpbm.Netpbm: NumPy matrix with modified channels.
     """
-    k = image.k
     M = image.M
-
     M = colorspace.normalize(M, 'RGB')
     M = colorspace.apply_to_channels(M, f_R, f_G, f_B)
     M = colorspace.denormalize(M, 'RGB')
 
-    # M = colorspace.RGB_to_Lab(M)
-    # M = colorspace.normalize(M, 'Lab')
-    # M = colorspace.apply_to_channels(M, f_R, f_G, f_B)
-    # M = colorspace.denormalize(M, 'Lab')
-    # M = colorspace.Lab_to_RGB(M)
-
-    # M = colorspace.RGB_to_YUV(M)
-    # M = colorspace.normalize(M, 'YUV')
-    # M = colorspace.apply_to_channels(M, f_R, f_G, f_B)
-    # M = colorspace.denormalize(M, 'YUV')
-    # M = colorspace.YUV_to_RGB(M)
-
-    return netpbm.Netpbm(P=image.P, k=k, M=M)
+    return netpbm.Netpbm(P=image.P, k=image.k, M=M)
 
 
 # COMPILE PIECES | 2021-03-29
 
-# functions
 identity = (lambda x: x, "x")
 invert = (lambda x: 1 - x, "1-x")
 zero = (lambda x: 0, "0")
@@ -65,7 +50,7 @@ def abs_sin_sft(p):
 
 
 def ceiling(p):
-    return (lambda x: ceil(x*p)/p, "frac{ceil(x*%d)}{%d}" % (p,p))
+    return (lambda x: np.ceil(x*p)/p, "ceil(x*%d)_over_%d" % (p,p))
 
 
 def triangle(a,p):
@@ -77,39 +62,18 @@ def multiply(p):
     return (lambda x: x*p, "%0.1fx" % p)
 
 
-# 2021-03-29 -- Normal RGB edits
-# pieces = [('math', abs_sin(5), identity, identity),
-#           ('math', abs_sin(10), identity, identity),
-#           ('math', identity, abs_sin(5), identity),
-#           ('math', identity, identity, abs_sin(10)),
-#           ('math', abs_sin(10), identity, abs_sin_sft(10)),
-#           ('math', ceiling(5), identity, identity),
-#           ('math', triangle(1.0,0.4), identity, identity),
-#           ('math', identity, identity, triangle(1.0,0.4)),
-#           ('math', invert, identity, identity),
-#           ('math', invert, invert, invert)]
+pieces = [('math', abs_sin(5), identity, identity),
+          ('math', abs_sin(10), identity, identity),
+          ('math', identity, abs_sin(5), identity),
+          ('math', identity, identity, abs_sin(10)),
+          ('math', abs_sin(10), identity, abs_sin_sft(10)),
+          ('math', ceiling(5), identity, identity),
+          ('math', triangle(1.0,0.4), identity, identity),
+          ('math', identity, identity, triangle(1.0,0.4)),
+          ('math', invert, identity, identity),
+          ('math', invert, invert, invert)]
 
-
-# 2021-03-31 -- Lab edits
-pieces = [('math', identity, zero, zero),
-          ('math', zero, identity, zero),
-          ('math', zero, zero, identity),
-          ('barn', identity, zero, zero),
-          ('barn', zero, identity, zero),
-          ('barn', zero, zero, identity),
-          ('math', identity, half, half),
-          ('math', zero, identity, half),
-          ('math', zero, half, identity),
-          ('barn', identity, half, half),
-          ('barn', zero, identity, half),
-          ('barn', zero, half, identity),
-          ('math', multiply(1.3), identity, identity),
-          ('math', identity, multiply(1.3), identity),
-          ('math', identity, identity, multiply(1.3)),
-          ('barn', multiply(1.3), identity, identity),
-          ('barn', identity, multiply(1.3), identity),
-          ('barn', identity, identity, multiply(1.3))]
-
+works = []
 for name, R, G, B in pieces:
     f_R, f_R_s = R
     f_G, f_G_s = G
@@ -118,3 +82,8 @@ for name, R, G, B in pieces:
     image = channel(image, f_R, f_G, f_B)
     path = "%s_channel_%s_%s_%s.ppm" % (name, f_R_s, f_G_s, f_B_s)
     image.to_netpbm(path)
+    works.append(path)
+
+with open("works.txt", "w") as f:
+    for work in works:
+        f.write("%s\n" % work)
