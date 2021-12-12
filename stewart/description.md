0cf06adbbc32b9ebb88207d5669c7d4f5fea340d
Henry Robbins <hwr26@cornell.edu>
2021-12-12 01:59:48 -0500

    Restructured stewart series directory.

--- stewart/description.md
+++ stewart/description.md
@@ -1,7 +1,3 @@
-**Henry Robbins**<br/>
-*stewart* (2021-04-14)<br/>
-MPEG-4
-
 Photos of Stewart Park in Ithaca, NY were taken and processed to produce a stop
 motion animation. Frames of this animation were transformed using techniques
 from the prior works mod and clip in addition to applying functions to various
--- stewart/stewart.py
+++ stewart/stewart.py
@@ -1,7 +1,8 @@
+import os
 import numpy as np
 from numpy import pi
 from scipy import signal
-from dmtools import colorspace
+from dmtools import colorspace, adjustments
 from dmtools.animation import clip, to_mp4
 from dmtools import sound
 import logging
@@ -12,33 +13,39 @@ logging.basicConfig(filename='stewart.log',
 
 def mod(M,k):
     M = colorspace.RGB_to_gray(M)
-    M = np.array(list(map(lambda x: x % k, M)))*int(256/64)
+    M = np.array(list(map(lambda x: x % k, M)))*int(256/k)
     return colorspace.gray_to_RGB(M)
 
 
 def clip_gradient(M,lb,ub):
     M = colorspace.RGB_to_gray(M)
-    M_lb = np.where(lb <= M, 1, 0)
-    M_ub = np.where(M <= ub, 1, 0)
-    M = np.where(M_lb + M_ub == 2, 0, 255)
+    M_lb = np.where(lb / 255 <= M, 1, 0)
+    M_ub = np.where(M <= ub / 255, 1, 0)
+    M = np.where(M_lb + M_ub == 2, 0, 1)
     return colorspace.gray_to_RGB(M)
 
 
 def channel(M,f1,f2,f3,color_space='RGB'):
     if color_space == 'RGB':
         M = colorspace.normalize(M, 'RGB')
-        M = colorspace.apply_to_channels(M, f1, f2, f3)
+        M = adjustments.apply_curve(M, f1, 0)
+        M = adjustments.apply_curve(M, f2, 1)
+        M = adjustments.apply_curve(M, f3, 2)
         M = colorspace.denormalize(M, 'RGB')
     elif color_space == 'YUV':
         M = colorspace.RGB_to_YUV(M)
         M = colorspace.normalize(M, 'YUV')
-        M = colorspace.apply_to_channels(M, f1, f2, f3)
+        M = adjustments.apply_curve(M, f1, 0)
+        M = adjustments.apply_curve(M, f2, 1)
+        M = adjustments.apply_curve(M, f3, 2)
         M = colorspace.denormalize(M, 'YUV')
         M = colorspace.YUV_to_RGB(M)
     elif color_space == 'Lab':
         M = colorspace.RGB_to_Lab(M)
         M = colorspace.normalize(M, 'Lab')
-        M = colorspace.apply_to_channels(M, f1, f2, f3)
+        M = adjustments.apply_curve(M, f1, 0)
+        M = adjustments.apply_curve(M, f2, 1)
+        M = adjustments.apply_curve(M, f3, 2)
         M = colorspace.denormalize(M, 'Lab')
         M = colorspace.Lab_to_RGB(M)
     else:
@@ -50,7 +57,10 @@ def shift(frames, shift):
     skip = shift
     for frame in frames:
         b = skip % 255
-        tmp.append(np.hstack((frame[:,b:,:],frame[:,:b,:])))
+        if len(frame.shape) == 2:
+            tmp.append(np.hstack((frame[:,b:],frame[:,:b])))
+        else:
+            tmp.append(np.hstack((frame[:,b:,:],frame[:,:b,:])))
         skip += shift
     return tmp
 
@@ -66,13 +76,13 @@ def shift(frames, shift):
 
 animations = []
 
-frames = (clip("car_ride") +
-          clip("pinches") +
-          clip("henry_movement") +
-          clip("ella_movement") +
-          clip("destroy_town"))
+frames = (clip("input/car_ride") +
+          clip("input/pinches") +
+          clip("input/henry_movement") +
+          clip("input/ella_movement") +
+          clip("input/destroy_town"))
 
-animations.append(([mod(f,64) for f in frames], 'mod', None, None))
+# animations.append(([mod(f,64) for f in frames], 'mod', None, None))
 animations.append(([clip_gradient(f,0,100) for f in frames], 'clip', None, None))
 
 def f1(p):
@@ -89,7 +99,7 @@ def f3():
 animations.append(([channel(f,f1(5),f2(12,5),f3=f3()) for f in frames], 'rgb_chaos', None, None))
 
 
-pinches_clip = clip("pinches")
+pinches_clip = clip("input/pinches")
 b = [0,5,10,14,18,22,28,35,40,45,49,53,57,61,66]
 clips = [pinches_clip[b[i]+1:b[i+1]] for i in range(len(b)-1)]
 frames = ([channel(f, f1(5), f2(12,5), f3()) for f in clips[0]] +
@@ -110,30 +120,30 @@ animations.append((frames,'pinches_chaos',None,None))
 
 
 plate_frames = [channel(f,lambda x: x,lambda x: 0.55, lambda x: 0.6, 'Lab')
-                for f in clip("plate")]
+                for f in clip("input/plate")]
 frames = (
     [np.zeros((189,255,3))]*10 +
-    shift([channel(f,f3(),f3(),f3()) for f in clip("town_tour")], 1) +
-    [mod(f,64) for f in clip("pinches", 29, 35)] +
-    shift([clip_gradient(f,100,200) for f in clip("henry_movement")], 20) +
-    [channel(f,f1(0),f2(16,5),f3()) for f in clip("pinches", 35, 40)] +
-    shift([clip_gradient(f,100,200) for f in clip("ella_movement")], 20) +
-    shift([channel(f,f3(),f3(),f3()) for f in clip("destroy_town", 0, 19)], 1) +
-    [clip_gradient(f,100,200) for f in clip("pinches", 0, 5)] +
-    shift([channel(f,f3(),f3(),f3()) for f in clip("destroy_town", 18, 44)[::-1]], -5) +
+    shift([channel(f,f3(),f3(),f3()) for f in clip("input/town_tour")], 1) +
+    [mod(f,64) for f in clip("input/pinches", 29, 35)] +
+    shift([clip_gradient(f,100,200) for f in clip("input/henry_movement")], 20) +
+    [channel(f,f1(0),f2(16,5),f3()) for f in clip("input/pinches", 35, 40)] +
+    shift([clip_gradient(f,100,200) for f in clip("input/ella_movement")], 20) +
+    shift([channel(f,f3(),f3(),f3()) for f in clip("input/destroy_town", 0, 19)], 1) +
+    [clip_gradient(f,100,200) for f in clip("input/pinches", 0, 5)] +
+    shift([channel(f,f3(),f3(),f3()) for f in clip("input/destroy_town", 18, 44)[::-1]], -5) +
     [channel(f,lambda x: x,lambda x: 0.5, lambda x: 0.6, 'Lab')
-        for f in clip("pinches", 40, 45)] +
-    shift([mod(f,64) for f in clip("destroy_town", 18, 44)], 5) +
-    [mod(f,32) for f in clip("pinches", 49, 53)] +
+        for f in clip("input/pinches", 40, 45)] +
+    shift([mod(f,64) for f in clip("input/destroy_town", 18, 44)], 5) +
+    [mod(f,32) for f in clip("input/pinches", 49, 53)] +
     shift(plate_frames[15:29][::-1] +
         plate_frames[15:] +
         plate_frames[24:29][::-1] +
         plate_frames[24:], 8) +
-    [clip_gradient(f,0,100) for f in clip("pinches", 22, 28)] +
-    shift([channel(f,f3(),f3(),f3()) for f in clip("tree_flowers", 4, 22)], 10) +
-    shift([channel(f,f3(),f3(),f3()) for f in clip("tree_flowers", 4, 22)[::-1]], -10) +
+    [clip_gradient(f,0,100) for f in clip("input/pinches", 22, 28)] +
+    shift([channel(f,f3(),f3(),f3()) for f in clip("input/tree_flowers", 4, 22)], 10) +
+    shift([channel(f,f3(),f3(),f3()) for f in clip("input/tree_flowers", 4, 22)[::-1]], -10) +
     [channel(f,lambda x: x,lambda x: 0.55, lambda x: 0.6, 'Lab')
-        for f in clip("pinches", 10, 14)] +
+        for f in clip("input/pinches", 10, 14)] +
     [np.zeros((189,255,3))]*10
 )
 
@@ -162,13 +172,7 @@ wave = wave / np.max(np.absolute(wave))
 animations.append((frames, 'film', sound.WAV(r=wave, l=wave), fps))
 
 
-works = []
+os.makedirs('output', exist_ok=True)
 for frames, name, audio, fps in animations:
-    works.append("stewart_%s.mp4" % name)
-    path = "stewart_%s.mp4" % name
+    path = "output/stewart_%s.mp4" % name
     to_mp4(frames=frames, audio=audio, path=path, fps=fps, s=3)
-
-with open("works.txt", "w") as f:
-    for work in works:
-        f.write("%s\n" % work)
-
