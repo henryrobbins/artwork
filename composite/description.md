808777b5ed79bbb7837ac29ea60a825d0f34c27d
Henry Robbins <hwr26@cornell.edu>
2022-01-25 13:27:52 -0500

    Published composite work series.

--- composite/description.md
+++ composite/description.md
@@ -1,1 +1,5 @@
-TODO: Add description
+Like the weierstrass series, this work explores the result of substituting
+carefully chosen parameters in image transformation algorithms with unbased
+choices. Here, arbitrary functions are used in the compositing of both the
+color and alpha (opacity) components of an image. This is referred to as
+[alpha compositing](https://en.wikipedia.org/wiki/Alpha_compositing).
--- composite/src.py
+++ composite/src.py
@@ -1,62 +1,75 @@
 import os
 import numpy as np
 import dmtools
-from dmtools import transform, colorspace, arrange
+from dmtools import transform, colorspace
+from dmtools.transform import CompositeOp, CompositeOpName
+from typing import Callable
 
 
 def safe_divide(n: np.ndarray, d: np.ndarray) -> np.ndarray:
     return np.divide(n, d, out=np.zeros_like(n), where=(d != 0))
 
 
-def alpha_composite(aA, aB) -> np.ndarray:
+def alpha_composite_1(aA, aB) -> np.ndarray:
+    return np.sin(aA) * np.sin(aB)
+
+
+def alpha_composite_2(aA, aB) -> np.ndarray:
+    return np.abs(np.sin(aA)) * np.abs(np.sin(aB))
+
+
+def alpha_composite_3(aA, aB) -> np.ndarray:
     return np.clip(aA + aB, 0, 1)
 
 
-def color_composite(xA, aA, xB, aB, xaA, xaB, aR) -> np.ndarray:
-    return safe_divide(xaA * transform.normalize(xaB*5), aR)
+def color_composite_1(xA, aA, xB, aB, xaA, xaB, aR) -> np.ndarray:
+    return safe_divide(xaA - xaB, aR)
 
 
-def f(source:np.ndarray, dest:np.ndarray) -> np.ndarray:
-    source = transform.rescale(source, 0.5)
-    dest = transform.rescale(dest, 0.5)
-    # source = colorspace.add_alpha(source, 0.5)
-    # dest = colorspace.add_alpha(dest, 0.5)
+def color_composite_2(xA, aA, xB, aB, xaA, xaB, aR) -> np.ndarray:
+    return safe_divide(xaA * transform.clip(xaB*5), aR)
+
+
+def f(source:np.ndarray, dest:np.ndarray, alpha_composite: Callable,
+    color_composite: Callable, background_color: int, post: Callable) -> np.ndarray:
+    # change alpha to 0.5 for both images
     source[:,:,3] = 0.5*source[:,:,3]
     dest[:,:,3] = 0.5*dest[:,:,3]
-    # source = colorspace.add_alpha(source, 0.5)
 
+    # create a gray background
     n,m,*_ = source.shape
-    background = np.ones((n,m,3)) * (150 / 255)
+    background = np.ones((n,m,3)) * (background_color / 255)
     background = colorspace.add_alpha(background, 1)
 
-    masks = [(0.0, 1.0, 0.3, 0.2),
-             (0.2, 0.6, 0.5, 0.45),
-             (0.9, 0.4, 0.1, 0.4),
-             (0.65, 0.85, 0.2, 0.15)]
+    # composite the two images and the composite on background
+    compsite_op = CompositeOp(alpha_composite, color_composite)
+    image = transform.composite(source, dest, operator=compsite_op)
+    image = transform.composite(image, background, CompositeOpName.ADD)
 
-    for x, y, w, h in masks:
-        source_sub = transform.crop(source,x,y,w,h,relative=True)
-        dest_sub = transform.crop(dest,x,y,w,h,relative=True)
-        patch = transform.composite(source_sub, dest_sub,
-                                    alpha_composite_function=alpha_composite,
-                                    color_composite_function=color_composite)
-        source = transform.substitute(source, patch, x, y, relative=True)
-
-    image = transform.composite(source, background, 'add')
-    image = transform.normalize(image)
-    image = arrange.border(image, 200, 1)
+    # post-processing (if any)
+    if post is not None:
+        image = post(image)
 
     return image
 
 
-# COMPILE PIECES | XXXX-XX-XX
-
-pieces = [('taughannock_1', 'taughannock_2')]
+pieces = [
+    ('road_2', 'road_sunset', alpha_composite_1, color_composite_1, 40, transform.clip),
+    ('road_2', 'road_sunset', alpha_composite_1, color_composite_1, 40, None),
+    ('peeling', 'erosion', alpha_composite_2, color_composite_1, 70, transform.clip),
+    ('erosion', 'peeling', alpha_composite_2, color_composite_1, 70, transform.clip),
+    ('road_sunset', 'taughannock_1', alpha_composite_2, color_composite_1, 70, transform.clip),
+    ('taughannock_1', 'road_sunset', alpha_composite_3, color_composite_2, 150, transform.normalize),
+    ('taughannock_1', 'peeling', alpha_composite_2, color_composite_1, 70, transform.clip)
+]
 
 os.makedirs('output', exist_ok=True)
-for source, dest in pieces:
+for source, dest, alpha, color, bg, post in pieces:
     A = dmtools.read(f"input/{source}.png")
     B = dmtools.read(f"input/{dest}.png")
-    image = f(A, B)
-    path = f"output/{source}_{dest}_composite.png"
-    dmtools.write_png(image, path, versioning=True)
+    image = f(A, B, alpha, color, bg, post)
+    if post is None:
+        path = f"output/{source}_{dest}_composite.png"
+    else:
+        path = f"output/{source}_{dest}_{post.__name__}_composite.png"
+    dmtools.write_png(image, path)
